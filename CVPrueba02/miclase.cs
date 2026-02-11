adcc9e8626a0f21f096fbe288d55f0699a31da09
mpozait <mpozait812@g.educaand.es>
2026-02-11 09:04:29 +0100

    Update miclase.cs

--- CVPrueba02/miclase.cs
+++ CVPrueba02/miclase.cs
@@ -8,5 +8,17 @@ namespace CVPrueba02
 {
     public class miclase
     {
+        //CONSTANTES - Aspectos de confg. de la clase
+
+        //MIEMBROS / CAMPOS - Agrupar datos protegidos (no accesibles desde fuera de la clase)
+        // NOMENCLATURA: _nombreCampo
+
+        //CONSTRUCTORES - Metodos especiales que definen la forma de instanciar objetos de la clase 
+        // Si no se definen constructores, la clase define uno por defecto ej: miclase()
+
+        //PROPIEDADES - Interfaz que forece la clase para controlar el acceso a los campos protegidos
+        //  Implementa los métodos get y set para obtener y establecer los datos asociados
+
+        //MÉTODOS - Publicos / Privados
     }
 }
