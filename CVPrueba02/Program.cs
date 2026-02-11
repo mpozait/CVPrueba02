fd9f75acb93a0d09aa06f6b83c807cb5bf8e7e42
Mehdi Del Pozo Ait el Hend <mpozait812@m.educaand.es>
2026-02-11 09:21:24 +0100

    Cambios

--- CVPrueba02/Program.cs
+++ CVPrueba02/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            
+            //CONSTANTES
+
+            //RECURSOS
+
+            //INICIALIZACIÓN
+
+            //ENTRADA
+
+            //PROCESOS
+
+            //SALIDA
         }
     }
 }
